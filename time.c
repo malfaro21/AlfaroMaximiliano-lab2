91d8786f32a06065bfe5ab3bb85b4854eca98a16
malfaro21 <malfaro21@students.desu.edu>
2023-10-25 04:03:08 +0000

    time attempt

--- time.c
+++ time.c
@@ -2,6 +2,7 @@
 
 double elapsed_time(struct timeval* start_time, struct timeval* end_time){
     //TODO: return the difference between end_time and start_time.
+    double elapsed_seconds = (double)(end_time->tv_sec - start_time->tv_sec);
     
     return 0.0;
 }
